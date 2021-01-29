1eec62559dc5c9b617c3714a7e1bea6bbe183079
Pina <fopina@gmail.com>
2021-01-29 09:51:07 +0000

    x

--- coil/__init__.py
+++ coil/__init__.py
@@ -1,6 +1,6 @@
 __program__ = 'coil'
 __version__ = '0.0.1'
-__description__ = 'Push notifications straight to your phone using multiple services'
+__description__ = 'Let it hatch, watch it coil (your python virtualenv)'
 
 import os.path
 import importlib
