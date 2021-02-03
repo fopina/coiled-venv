b5103b65641a4639376a1b8d0b88722d9ee72bf1
Filipe Pina <63779195+fopinappb@users.noreply.github.com>
2021-02-03 23:58:17 +0000

    x

--- coil/__main__.py
+++ coil/__main__.py
@@ -6,14 +6,8 @@ import platform
 import json
 from pathlib import Path
 
-__platform = platform.system()
-ON_MACOS = os.name == 'mac' or __platform == 'Darwin'
-ON_WINDOWS = os.name == 'nt' or __platform == 'Windows'
-
-# TODO: make this configurable
-VENV_DIR = Path.home() / '.virtualenvs'
-CONF_DIR = Path.home() / '.coil'
-ENV_DB = CONF_DIR / 'mapping.json'
+from .config import ON_WINDOWS, VENV_DIR, CONF_DIR, ENV_DB
+from . import db, venv
 
 
 def parseargs(args):
@@ -30,84 +24,31 @@ def parseargs(args):
     return parser.parse_args(args)
 
 
-def venv_bin(path):
-    d = path / 'bin'
-    if d.is_dir():
-        return d
-    if ON_WINDOWS:
-        d = path / 'Scripts'
-        if d.is_dir():
-            return d
-    return None
 
-
-def is_venv(path):
-    return venv_bin(path) is not None
-
-
-def list_environments():
-    for l in VENV_DIR.glob('*'):
-        if is_venv(l):
-            print(l.name)
-    
-
-def env_for_path(path=None):
-    if path is None:
-        path = Path.cwd()
-    return None
-
-
-def load_env(path):
-    print('LOADING')
-
-
-def setup_conf():
-    CONF_DIR.mkdir(exist_ok=True)
-
-
-class DB(dict):
-    def __init__(self, *a, **b):
-        super().__init__(self, *b, **b)
-
-    def load(self):
-        self.clear()
-        try:
-            with open(ENV_DB) as f:
-                self.update(json.load(f))
-        except FileNotFoundError:
-            pass
-
-    def dump(self):
-        with open(ENV_DB, 'wb') as f:
-            json.dump(self, f)
-
-
-def bind_env(name):
-    setup_conf()
-    db = DB()
-    db['x'] = 1
-    db.dump()
+class CommandError(Exception):
+    pass
 
 
 def main(args=None):
     args = parseargs(args)
     if args.list:
-        return list_environments()
-    
+        for _e in venv.list_available():
+            print(_e)
+        return 0
+
     if args.bind:
-        env = bind_env(args.bind)
+        env = venv.bind(args.bind)
         if env is None:
             print(f'env {args.bind} does not exist. Use -c instead to create it or -l to see the ones available')
             return 1
-        load_env(env)
+        venv.load(env)
 
-    env = env_for_path()
+    env = venv.for_path()
     if env is None:
         print('No env configured for current directory. Use "-b ENV" to bind an existing one or "-c ENV" to create and bind to a new one')
         return 1
     
-    load_env(env)
-    # no return needed: exec or Exception
+    venv.load(env)
 
 
 if __name__ == '__main__':
